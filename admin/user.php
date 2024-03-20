ef44fb336ae5528cebad4c2e6cef5b9fc54e96a5
hey991 <70498798+hey991@users.noreply.github.com>
2024-03-20 18:32:48 +0530

    Add files via upload

---
+++ admin/user.php
@@ -0,0 +1,81 @@
+<?php include('session.php'); ?>
+<?php include('header.php'); ?>
+<body>
+<?php include('navbar.php'); ?>
+<div class="container">
+	<div class="row">
+		<?php include('userlist.php'); ?>
+	</div>
+</div>
+
+<?php include('cruduser_modal.php'); ?>
+<?php include('modal.php'); ?>
+
+<script src="../js/jquery.dataTables.min.js"></script>
+<script src="../js/dataTables.bootstrap.min.js"></script>
+<script src="../js/dataTables.responsive.js"></script>
+<script>
+$(document).ready(function(){
+	
+	
+	$('#userList').DataTable({
+	"bLengthChange": true,
+	"bInfo": true,
+	"bPaginate": true,
+	"bFilter": true,
+	"bSort": false,
+	"pageLength": 7
+	});
+	
+	$(document).on('click', '#adduser', function(){
+		name=$('#uname').val();
+		username=$('#uusername').val();
+		password=$('#upassword').val();
+		access=$('#uaccess').val();
+			$.ajax({
+				url:"add_user.php",
+				method:"POST",
+				data:{
+					name: name,
+					username: username,
+					password: password,
+					access: access,
+					adduser: 1,
+				},
+				success:function(){
+					window.location.href='user.php';
+				}
+			});
+		
+	});
+	//
+	$(document).on('click', '.deleteuser', function(){
+		var rid=$(this).val();
+		$('#delete_user').modal('show');
+		$('.modal-footer #confirm_delete').val(rid);
+	});
+	
+	$(document).on('click', '#confirm_delete', function(){
+		var nrid=$(this).val();
+		$('#delete_user').modal('hide');
+		$('body').removeClass('modal-open');
+		$('.modal-backdrop').remove();
+			$.ajax({
+				url:"deleteuser.php",
+				method:"POST",
+				data:{
+					id: nrid,
+					del: 1,
+				},
+				success:function(){
+					window.location.href='user.php';
+				}
+			});
+	});
+	
+	$(document).on('click', '.edituser', function(){
+		var rid=$(this).val();
+		var name=$('#ename'+rid).val();
+		var username=$('#eusername'+rid).val();
+		var password=$('#epassword'+rid).val();
+		
